3d9db5a24af6089ad6a44ea28a715850247de8ac
Abraham <abrahann.lg@gmail.com>
2026-07-19 22:51:21 -0600

    Actualización Yolo

--- src/services/yolo_service.py
+++ src/services/yolo_service.py
@@ -1,27 +1,34 @@
 import time
 import queue
 from src.core.base_service import BaseService
+from ultralytics import YOLO
 
-class yolo_service(BaseService):
+class YoloService(BaseService):
     def __init__(self, shared_state, config):
         super().__init__(shared_state, config)
 
-        self.model_path = self.config["software"]["ai_model"]["model_path"]
-        self.frame_queue = self.shared_sate.get_volatile("camera_frame_queue")  # gets image -> queue.get()
+        self.model = YOLO(self.config.get("software",{}).get("ai_model", {}).get("model_path","data/models/best.pt"))
+        self.frame_queue = self.shared_state.get_volatile("camera_frame_queue")  # gets image -> queue.get()
+        self.conf_threshold = self.config.get("software",{}).get("ai_model",{}).get("confidence_threshold", 0.85)
 
-        def _main_loop(self):
-            while not self._stop_event.is_set():
-                try:
-                    results = self.model_path(self.frame_queue.get(timeout=1) )     # We took a frame from the tail (the last one)
+    def _main_loop(self):
+        while not self._stop_event.is_set():
+            try:
+                results = self.model(self.frame_queue.get(timeout=1) )          # We took a frame from the tail (the last one)
 
-                    detected = len(results[0].boxes) > 0        # If it has a length greater than zero, it means it found an object
+                detected = any(box.conf[0] >= self.conf_threshold for box in results[0].boxes)
 
-                    self.shared_state.set_volatile("person_detected", detected)     # We assign the detection to person_detected
+                self.shared_state.set_volatile("person_detected", detected)     # We assign the detection to person_detected
 
-                    self.report_health()                        # We made sure everything went well
-                except:
-                    self.logger.warning("Report error in Yolo Service")
-                    self.report_error()
-                    pass
+                self.report_health()                                            # We made sure everything went well
+
+                time.sleep(1)                                                   # throttle
+            
+            except queue. Empty:                                                # Frame queue vacía es normal, no es un errorreal
+                pass
+            except Exception as e:
+                self.logger.error(f"Unexpected error in YOLOservice: {e}")
+                self.report_error()
+                time.sleep(1)
 
 
