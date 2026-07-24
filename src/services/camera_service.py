1f85421090e172299155a9eb3b39fb493fc6b66a
schoenstatt13 <schoenstatt04@gmail.com>
2026-07-24 02:22:15 -0600

    camera service: correcciones e implementacion de nueva dinamica queue

--- src/services/camera_service.py
+++ src/services/camera_service.py
@@ -22,6 +22,10 @@ class cameras_service(BaseService):
         
         self.previous_frame_uno = None
         self.previous_frame_dos = None
+        self.previous_zone_a1 = None
+        self.previous_zone_a2 = None
+        self.previous_zone_b1 = None
+        self.previous_zone_b2 = None
         #self.movement_threshold = self.config["software"]["camera_service"]["movement_threshold"]
         self.movement_threshold = self.config.get("software", {}).get("camera_service", {}).get("movement_threshold", 5.0)
 
@@ -85,28 +89,83 @@ class cameras_service(BaseService):
                     continue
 
                 if not camera_uno_frozen:
-                    for zone_name, zone_data in self.cameras_config["camera1"]["zones"].items():
-                        x_min, y_min, x_max, y_max = zone_data["coords"]
-                        cropped = frame_uno[y_min:y_max, x_min:x_max]
+                    zonea1_data = self.cameras_config["camera1"]["zones"]["zone_a1"]
+                    x_min_a1, y_min_a1, x_max_a1, y_max_a1 = zonea1_data["coords"]
 
-                        self.frame_queue.put_nowait({
-                            "camera_id":"camera1",
-                            "zone": zone_name,
-                            "timestamp": time.time(),
-                            "frame": cropped,
-                        })
+                    cropped_a1 = frame_uno[y_min_a1:y_max_a1, x_min_a1:x_max_a1]
+
+                    gray_cropped_a1 = cv2.cvtColor(cropped_a1, cv2.COLOR_BGR2GRAY)
+
+                    if self.previous_zone_a1 is not None:
+                        diff_a1 = cv2.absdiff(gray_cropped_a1, self.previous_zone_a1)
+
+                        if np.mean(diff_a1) > self.movement_threshold:
+
+                            if self.frame_queue.full():
+                                try:
+                                    self.frame_queue.get_nowait()
+                                except queue.Empty:
+                                    pass
+
+                            self.frame_queue.put_nowait({
+                                "camera_id":"camera1",
+                                "zone":"zone_a1",
+                                "timestamp": time.time(),
+                                "frame": cropped_a1,
+                            })
+
+                    self.previous_zone_a1 = gray_cropped_a1
+
+                    zonea2_data = self.cameras_config["camera1"]["zones"]["zone_a2"]
+                    x_min_a2, y_min_a2, x_max_a2, y_max_a2 = zonea2_data["coords"]
+
+                    cropped_a2 = frame_uno[y_min_a2:y_max_a2, x_min_a2:x_max_a2]
+
+                    gray_cropped_a2 = cv2.cvtColor(cropped_a2, cv2.COLOR_BGR2GRAY)
+
+                    if self.previous_zone_a2 is not None:
+                        diff_a2 = cv2.absdiff(gray_cropped_a2, self.previous_zone_a2)
+
+                        if np.mean(diff_a2) > self.movement_threshold:
+                            self.logger.info("Movement in Camera 1 zone a2")
+                    
+                    self.previous_zone_a2 = gray_cropped_a2
+
+                    
 
                 if not camera_dos_frozen:
-                    for zone_name, zone_data in self.cameras_config["camera2"]["zones"].items():
-                        x_min, y_min, x_max, y_max = zone_data["coords"]
-                        cropped = frame_dos[y_min:y_max, x_min:x_max]
+                    zoneb1_data =  self.cameras_config["camera2"]["zones"]["zone_b1"]
+                    x_min_b1, y_min_b1, x_max_b1, y_max_b1 = zoneb1_data["coords"]
 
-                        self.frame_queue.put_nowait({
-                            "camera_id": "camera2",
-                            "zone": zone_name,
-                            "timestamp": time.time(),
-                            "frame": cropped
-                        })
+                    cropped_b1 = frame_dos[y_min_b1:y_max_b1, x_min_b1:x_max_b1]
+
+                    gray_cropped_b1 = cv2.cvtColor(cropped_b1, cv2.COLOR_BGR2GRAY)
+
+                    if self.previous_zone_b1 is not None:
+                        diff_b1 = cv2.absdiff(gray_cropped_b1, self.previous_zone_b1)
+
+                        if np.mean(diff_b1) > self.movement_threshold:
+                            self.logger.info("Movement detected in Camera 2 zone b1")
+                    
+                    self.previous_zone_b1 = gray_cropped_b1
+
+                    zoneb2_data =  self.cameras_config["camera2"]["zones"]["zone_b2"]
+                    x_min_b2, y_min_b2, x_max_b2, y_max_b2 = zoneb2_data["coords"]
+
+                    cropped_b2 = frame_dos[y_min_b2:y_max_b2, x_min_b2:x_max_b2]
+
+                    gray_cropped_b2 = cv2.cvtColor(cropped_b2, cv2.COLOR_BGR2GRAY)
+
+                    if self.previous_zone_b2 is not None:
+                        diff_b2 = cv2.absdiff(gray_cropped_b2, self.previous_zone_b2)
+
+                        if np.mean(diff_b2) > self.movement_threshold:
+                            self.logger.info("Movement detected in Camera 2 zone b2")
+
+                    self.previous_zone_b2 = gray_cropped_b2
+                    
+                    
+
                 
                 self.report_health()
                 time.sleep(0.03)
